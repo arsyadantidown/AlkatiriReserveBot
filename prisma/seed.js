aee3c082125adc475a0d9d04080fc04dd124a1fa
arsyadantidown <superciouskid@gmail.com>
2026-03-27 15:04:07 +0700

    Update seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -17,7 +17,7 @@ async function main() {
       { name: "Ayam Kemasan", quantity: 1000, maxStock: 2000 },
       { name: "Daging", quantity: 1000, maxStock: 2000 },
       { name: "Coklat", quantity: 1000, maxStock: 2000 },
-      { name: "Strawberry", quantity: 1000, maxStock: 2000 },
+      { name: "Berry", quantity: 1000, maxStock: 2000 },
       { name: "Soda", quantity: 1000, maxStock: 2000 },
       { name: "Air", quantity: 2000, maxStock: 3000 },
       { name: "Es Batu", quantity: 2000, maxStock: 3000 },
@@ -33,12 +33,13 @@ async function main() {
     data: [
       { name: "Creamy Mushroom Pasta", category: "food" },
       { name: "Butter Chicken Rice", category: "food" },
-      { name: "Roasted Chicken Provencale", category: "food" },
+      { name: "Pepper Crusted Portobello Steak", category: "food" },
       { name: "Chicken Cordon Blue", category: "food" },
       { name: "Tiramisu", category: "food" },
       { name: "Cheese Quiche", category: "food" },
 
-      { name: "Sparkling Water", category: "drink" },
+      { name: "Sparkling Water A", category: "drink" },
+      { name: "Sparkling Water B", category: "drink" },
       { name: "Jasmine Tea", category: "drink" },
       { name: "Iced Lemon Tea", category: "drink" },
       { name: "Iced Chocolate", category: "drink" },
@@ -68,19 +69,19 @@ await prisma.recipeIngredient.createMany({
     // ================= FOOD =================
 
     // Creamy Mushroom Pasta
-    { recipeId: recipeMap["Creamy Mushroom Pasta"], stockId: stockMap["Tepung"], amount: 10 },
-    { recipeId: recipeMap["Creamy Mushroom Pasta"], stockId: stockMap["Susu"], amount: 10 },
-    { recipeId: recipeMap["Creamy Mushroom Pasta"], stockId: stockMap["Garam"], amount: 10 },
+    { recipeId: recipeMap["Creamy Mushroom Pasta"], stockId: stockMap["Tepung"], amount: 5 },
+    { recipeId: recipeMap["Creamy Mushroom Pasta"], stockId: stockMap["Susu"], amount: 5 },
+    { recipeId: recipeMap["Creamy Mushroom Pasta"], stockId: stockMap["Garam"], amount: 5 },
 
     // Butter Chicken Rice
-    { recipeId: recipeMap["Butter Chicken Rice"], stockId: stockMap["Ayam Kemasan"], amount: 10 },
-    { recipeId: recipeMap["Butter Chicken Rice"], stockId: stockMap["Beras"], amount: 10 },
-    { recipeId: recipeMap["Butter Chicken Rice"], stockId: stockMap["Garam"], amount: 10 },
+    { recipeId: recipeMap["Butter Chicken Rice"], stockId: stockMap["Ayam Kemasan"], amount: 3 },
+    { recipeId: recipeMap["Butter Chicken Rice"], stockId: stockMap["Beras"], amount: 6 },
+    { recipeId: recipeMap["Butter Chicken Rice"], stockId: stockMap["Garam"], amount: 5 },
 
-    // Roasted Chicken Provencale
-    { recipeId: recipeMap["Roasted Chicken Provencale"], stockId: stockMap["Ayam Kemasan"], amount: 10 },
-    { recipeId: recipeMap["Roasted Chicken Provencale"], stockId: stockMap["Cabai"], amount: 10 },
-    { recipeId: recipeMap["Roasted Chicken Provencale"], stockId: stockMap["Beras"], amount: 10 },
+    // Pepper Crusted Portobello Steak
+    { recipeId: recipeMap["Pepper Crusted Portobello Steak"], stockId: stockMap["Daging"], amount: 4 },
+    { recipeId: recipeMap["Pepper Crusted Portobello Steak"], stockId: stockMap["Cabai"], amount: 4 },
+    { recipeId: recipeMap["Pepper Crusted Portobello Steak"], stockId: stockMap["Garam"], amount: 4 },
 
     // Chicken Cordon Blue
     { recipeId: recipeMap["Chicken Cordon Blue"], stockId: stockMap["Ayam Kemasan"], amount: 10 },
@@ -90,19 +91,24 @@ await prisma.recipeIngredient.createMany({
 
     // Tiramisu
     { recipeId: recipeMap["Tiramisu"], stockId: stockMap["Coklat"], amount: 5 },
-    { recipeId: recipeMap["Tiramisu"], stockId: stockMap["Susu"], amount: 5 },
+    { recipeId: recipeMap["Tiramisu"], stockId: stockMap["Susu"], amount: 3 },
 
     // Cheese Quiche
-    { recipeId: recipeMap["Cheese Quiche"], stockId: stockMap["Susu"], amount: 5 },
+    { recipeId: recipeMap["Cheese Quiche"], stockId: stockMap["Susu"], amount: 3 },
     { recipeId: recipeMap["Cheese Quiche"], stockId: stockMap["Garam"], amount: 2 },
-    { recipeId: recipeMap["Cheese Quiche"], stockId: stockMap["Daging"], amount: 5 },
+    { recipeId: recipeMap["Cheese Quiche"], stockId: stockMap["Daging"], amount: 2 },
 
     // ================= DRINK =================
 
-    // Sparkling Water
-    { recipeId: recipeMap["Sparkling Water"], stockId: stockMap["Soda"], amount: 5 },
-    { recipeId: recipeMap["Sparkling Water"], stockId: stockMap["Air"], amount: 5 },
-    { recipeId: recipeMap["Sparkling Water"], stockId: stockMap["Es Batu"], amount: 10 },
+    // Sparkling Water A
+    { recipeId: recipeMap["Sparkling Water A"], stockId: stockMap["Soda"], amount: 5 },
+    { recipeId: recipeMap["Sparkling Water A"], stockId: stockMap["Air"], amount: 4 },
+    { recipeId: recipeMap["Sparkling Water A"], stockId: stockMap["Es Batu"], amount: 4 },
+
+    // Sparkling Water B
+    { recipeId: recipeMap["Sparkling Water B"], stockId: stockMap["Soda"], amount: 4 },
+    { recipeId: recipeMap["Sparkling Water B"], stockId: stockMap["Air"], amount: 3 },
+    { recipeId: recipeMap["Sparkling Water B"], stockId: stockMap["Es Batu"], amount: 3 },    
 
     // Jasmine Tea
     { recipeId: recipeMap["Jasmine Tea"], stockId: stockMap["Teh"], amount: 5 },
@@ -111,20 +117,20 @@ await prisma.recipeIngredient.createMany({
     { recipeId: recipeMap["Jasmine Tea"], stockId: stockMap["Gula"], amount: 5 },
 
     // Iced Lemon Tea
-    { recipeId: recipeMap["Iced Lemon Tea"], stockId: stockMap["Teh"], amount: 10 },
-    { recipeId: recipeMap["Iced Lemon Tea"], stockId: stockMap["Air"], amount: 5 },
-    { recipeId: recipeMap["Iced Lemon Tea"], stockId: stockMap["Es Batu"], amount: 5 },
-    { recipeId: recipeMap["Iced Lemon Tea"], stockId: stockMap["Garam"], amount: 5 },
+    { recipeId: recipeMap["Iced Lemon Tea"], stockId: stockMap["Teh"], amount: 5 },
+    { recipeId: recipeMap["Iced Lemon Tea"], stockId: stockMap["Air"], amount: 4 },
+    { recipeId: recipeMap["Iced Lemon Tea"], stockId: stockMap["Es Batu"], amount: 4 },
+    { recipeId: recipeMap["Iced Lemon Tea"], stockId: stockMap["Garam"], amount: 4 },
 
     // Iced Chocolate
-    { recipeId: recipeMap["Iced Chocolate"], stockId: stockMap["Coklat"], amount: 5 },
-    { recipeId: recipeMap["Iced Chocolate"], stockId: stockMap["Es Batu"], amount: 5 },
-    { recipeId: recipeMap["Iced Chocolate"], stockId: stockMap["Air"], amount: 5 },
+    { recipeId: recipeMap["Iced Chocolate"], stockId: stockMap["Coklat"], amount: 3 },
+    { recipeId: recipeMap["Iced Chocolate"], stockId: stockMap["Es Batu"], amount: 3 },
+    { recipeId: recipeMap["Iced Chocolate"], stockId: stockMap["Air"], amount: 4 },
 
-    // Strawberry Infused Water
-    { recipeId: recipeMap["Strawberry Infused Water"], stockId: stockMap["Strawberry"], amount: 5 },
-    { recipeId: recipeMap["Strawberry Infused Water"], stockId: stockMap["Air"], amount: 2 },
-    { recipeId: recipeMap["Strawberry Infused Water"], stockId: stockMap["Es Batu"], amount: 5 }
+    // Triple Berry Soda
+    { recipeId: recipeMap["Triple Berry Soda"], stockId: stockMap["Berry"], amount: 4 },
+    { recipeId: recipeMap["Triple Berry Soda"], stockId: stockMap["Air"], amount: 2 },
+    { recipeId: recipeMap["Triple Berry Soda"], stockId: stockMap["Es Batu"], amount: 2 }
 
   ],
   skipDuplicates: true
